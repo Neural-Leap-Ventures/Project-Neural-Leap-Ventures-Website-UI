9ca9e58381bad51a77c4f6eab236600dc37e5a68
William-Boomer-Baker <boomer@boomerbaker.com>
2022-12-29 23:10:28 +0530

    Connect contact-us form with the db

---
+++ server/contactUs.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+require('./customFunctions/contactModel')
+const User = mongoose.model('contacts')
+const shortid = require('shortid')
+//const cookie = require('cookie')
+
+exports.handler = async (event, context) => {
+  const array = event.body.split('&')
+  const firstName = array[0].split('fname=')
+  const lastName = array[1].split('lname=')
+  const companyName = array[2].split('cname=')
+  const companySize = array[3].split('field=')
+  const phone = array[4].split('pnumber=')
+
+  try {
+    mongoose.connect(
+      'mongodb+srv://neuralleap:ElonMusk123@neuralleap.6zgols5.mongodb.net/?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      }
+    )
+
+    const shortIdVariable = shortid.generate()
+
+    const user = await new User({
+      referralId: shortIdVariable,
+      first_name: firstName[1],
+      last_name: lastName[1],
+      company_name: companyName[1],
+      company_size: companySize[1],
+      phone_number: phone[1],
+    })
+
+    await user.save()
+
+    return {
+      statusCode: 200,
+      body: 'Success',
+    }
+  } catch (err) {
+    return {
+      statusCode: 400,
+      body: err,
+    }
+  }
+}
